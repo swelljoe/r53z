bf42d64f8421df335abc7e1fb971ec60fd16f424
Joe Cooper <joe@virtualmin.com>
2016-06-13 12:13:30 -0700

    library cleanup

--- lib/r53z.rb
+++ lib/r53z.rb
@@ -1,10 +1,10 @@
 require_relative "r53z/version"
 require "methadone"
 require "aws-sdk"
-require_relative "config"
-require_relative "client"
-require_relative "file"
-require_relative "cli"
+require_relative "r53z/config"
+require_relative "r53z/client"
+require_relative "r53z/file"
+require_relative "r53z/cli"
 
 module R53z
 end
